d80ac42b67964107768101338eb60b5bbf209d71
Vinh Le <lethanhvinh95@gmail.com>
2023-09-13 07:45:06 +0300

    fetching GitHub profile

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,9 +1,27 @@
-import { PageProps } from '$fresh/server.ts'
+import { RouteContext } from "$fresh/server.ts";
 
-export default function Page(props: Omit<PageProps, 'params'> & { params: { name: string } }) {
+interface GitHubResponse {
+  login: string;
+  name: string;
+  avatar_url: string;
+}
+
+export default async function GitHubProfile(_req: Request, ctx: RouteContext) {
+  const resp = await fetch(
+    `https://api.github.com/users/${ctx.params.name}`,
+  );
+
+  if (!resp.ok) {
+    return <h1>An Error occurred</h1>;
+  }
+
+  const { login, name, avatar_url } = (await resp.json()) as GitHubResponse;
+
   return (
-    <main>
-      <h1>Hello {props.params.name}</h1>
-    </main>
-  )
+    <div>
+      <img src={avatar_url} width={64} height={64} />
+      <h1>{name}</h1>
+      <p>{login}</p>
+    </div>
+  );
 }
