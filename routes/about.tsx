d38fde80a3594c4946c086304e0db86cf8bf2e78
Vinh Le <lethanhvinh95@gmail.com>
2023-09-13 07:07:48 +0300

    add route handler

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,3 +1,13 @@
+import { Handlers } from '$fresh/server.ts'
+
+export const handler: Handlers = {
+  GET: async (req, ctx) => {
+    const res = await ctx.render()
+    res.headers.set('X-Custom-Header', 'Hello World')
+    return res
+  }
+}
+
 export default function AboutPage() {
   return (
     <main>
